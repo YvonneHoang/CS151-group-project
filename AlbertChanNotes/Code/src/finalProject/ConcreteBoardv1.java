8054b1d84769d6080f065f17ad7e2d353a6b732d
firen777 <freeze1995831@yahoo.com>
2017-04-21 02:51:31 -0700

    Added BoardStyle<i> a concrete style and a main class for testing

---
+++ AlbertChanNotes/Code/src/finalProject/ConcreteBoardv1.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package finalProject;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+
+/**
+ * @author UnHou Chan
+ *
+ */
+public class ConcreteBoardv1 implements BoardStyle {
+	
+	private final static int BOARD_WIDTH = 400;	//W
+	private final static int BOARD_HEIGHT= 150;	//H
+	private final static int PIT_DIAMETER= 50;	//d
+	private final static int MAN_WIDTH = 50;	//mW
+	private final static int MAN_HEIGHT= 150;	//mH
+
+	
+	private ArrayList<Shape> pitShapes = new ArrayList<Shape>();
+	private Shape mancalaShapesA;
+	private Shape mancalaShapesB;
+	private Shape boardShape;
+	
+	/**Initializing all Shapes that will be drawn
+	 * 
+	 */
+	public ConcreteBoardv1(){
+		/*Board*/
+		boardShape = new Rectangle2D.Double(0,0,BOARD_WIDTH, BOARD_HEIGHT);
+		
+		/*Mancala*/
+		mancalaShapesA = new Ellipse2D.Double(BOARD_WIDTH - MAN_WIDTH, 0, MAN_WIDTH, MAN_HEIGHT);
+		mancalaShapesB = new Ellipse2D.Double(0,0, MAN_WIDTH, MAN_HEIGHT);
+		
+		/*PlayerA's pit*/
+		for (int i=0;i<6;i++){
+			/* formula:
+			 * x = (i+1)*d
+			 * y = H-d
+			 * */
+			pitShapes.add(new Ellipse2D.Double((i+1)*PIT_DIAMETER, BOARD_HEIGHT-PIT_DIAMETER, PIT_DIAMETER, PIT_DIAMETER));
+		}
+		/*PlayerB's pit*/
+		for (int i=0;i<6;i++){
+			/* formula:
+			 * x = W - (i+2)*d
+			 * y = 0
+			 * */
+			pitShapes.add(new Ellipse2D.Double(BOARD_WIDTH-((i+2)*PIT_DIAMETER), 0, PIT_DIAMETER, PIT_DIAMETER));
+		}
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see finalProject.BoardStyle#drawBoard(java.awt.Graphics2D)
+	 */
+	@Override
+	public void drawBoard(Graphics2D g2) {
+		g2.setColor(new Color(0xaf8c56));
+		g2.draw(boardShape);
+		g2.fill(boardShape);
+	}
+
+	/* (non-Javadoc)
+	 * @see finalProject.BoardStyle#drawPit(int, int, java.awt.Graphics2D)
+	 */
+	@Override
+	public void drawPit(int pitID, int stones, Graphics2D g2) {
+		if (pitID<=11 && pitID >=0) {
+			g2.setColor(new Color(0xff0000));
+			g2.draw(pitShapes.get(pitID));
+			g2.fill(pitShapes.get(pitID));
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see finalProject.BoardStyle#drawMancala(int, int, java.awt.Graphics2D)
+	 */
+	@Override
+	public void drawMancala(int mID, int stones, Graphics2D g2) {
+		Shape temp = null;
+		if (mID == 0) 
+			temp = this.mancalaShapesA;
+		if (mID == 1)
+			temp = this.mancalaShapesB;
+		if (temp!= null) {
+			g2.setColor(new Color(0x00ffff));
+			g2.draw(temp);
+			g2.fill(temp);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see finalProject.BoardStyle#pitSelected(java.awt.Point)
+	 */
+	@Override
+	public int pitSelected(Point p) {
+		for (int i=0; i<12; i++){
+			if (this.pitShapes.get(i).contains(p))
+				return i;
+		}
+		return -1;
+	}
+
+	/* (non-Javadoc)
+	 * @see finalProject.BoardStyle#pitSelected(int, int)
+	 */
+	@Override
+	public int pitSelected(int x, int y) {
+		for (int i=0; i<12; i++){
+			if (this.pitShapes.get(i).contains(x, y))
+				return i;
+		}
+		return -1;
+	}
+
+}
---
+++ AlbertChanNotes/Code/src/finalProject/StyleTester.java
@@ -0,0 +1,67 @@
+package finalProject;
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+public class StyleTester extends JComponent{
+	
+	private BoardStyle boardStyle;
+	
+	public StyleTester() {
+		boardStyle = new ConcreteBoardv1();
+		
+		this.addMouseListener(new MouseAdapter(){
+
+			/* (non-Javadoc)
+			 * @see java.awt.event.MouseAdapter#mousePressed(java.awt.event.MouseEvent)
+			 */
+			@Override
+			public void mousePressed(MouseEvent e) {
+				Point p = e.getPoint();
+//				int x = e.getX();
+//				int y = e.getY();
+//				System.out.println(boardStyle.pitSelected(x, y));
+				System.out.println(boardStyle.pitSelected(p));
+			}
+			
+		});
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		// TODO Auto-generated method stub
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		boardStyle.drawBoard(g2);
+		boardStyle.drawMancala(1, 0, g2);
+		boardStyle.drawMancala(0, 0, g2);
+		for (int i=0; i<12; i++){
+			boardStyle.drawPit(i, 0, g2);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame();
+		frame.setLayout(new BorderLayout());
+		
+		StyleTester tester = new StyleTester();
+		
+		frame.add(tester);
+		frame.setSize(420, 300);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+}
